b58933222bde9019b3ab8cf6c6b7a052f568a9c5
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-11 12:51:32 +0200

    Update SetBalanceCommand.php

--- src/anybananagame/ottereco/commands/SetBalanceCommand.php
+++ src/anybananagame/ottereco/commands/SetBalanceCommand.php
@@ -8,13 +8,11 @@ use pocketmine\plugin\PluginOwnedTrait;
 use anybananagame\ottereco\Main;
 use pocketmine\utils\Config;
 
-class SetBalanceCommand extends Command implements PluginOwned
-{
-
+class SetBalanceCommand extends Command implements PluginOwned {
     use PluginOwnedTrait;
     private $con;
-    public function __construct(Main $plugin)
-    {
+    
+    public function __construct(Main $plugin) {
         $this->owningPlugin = $plugin;
         $this->con = $plugin;
         parent::__construct("setbalance", "Set someones balance", "/setbalance <player> <amount>");
@@ -22,16 +20,16 @@ class SetBalanceCommand extends Command implements PluginOwned
         $this->setDescription("Set Balance on user");
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
-    {
-        if(!isset($args[0])) {
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!isset($args[0])) {
             $message = $this->con->getConfig()->get("nousername");
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
             $message = str_replace("{cmd}", "setbalance", $message);
             $sender->sendMessage($message);
             return true;
         } 
-        if(!isset($args[1])) {
+
+        if (!isset($args[1])) {
             $message = $this->con->getConfig()->get("noamount");
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
             $message = str_replace("{cmd}", "setbalance", $message);
@@ -43,7 +41,8 @@ class SetBalanceCommand extends Command implements PluginOwned
     
         $lowuser = strtolower($args[0]);
         $data = $db->get("$lowuser");
-        if(!$data){
+        
+        if (!$data){
             $message = $this->con->getConfig()->get("userdoesnotexist");
             $message = str_replace("{username}", $lowuser, $message);
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
@@ -51,14 +50,16 @@ class SetBalanceCommand extends Command implements PluginOwned
             $sender->sendMessage($message);
             return false;
         }
-        if(!is_numeric($args[1])){
+        
+        if (!is_numeric($args[1])){
             $message = $this->con->getConfig()->get("NAN");
             $message = str_replace("{arg}", $args[1], $message);
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
             $sender->sendMessage($message);
              return false;
         }
-        if($args[1] < 0){
+        
+        if ($args[1] < 0){
             $message = $this->con->getConfig()->get("belowzero");
             $message = str_replace("{arg}", $args[1], $message);
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
@@ -73,8 +74,6 @@ class SetBalanceCommand extends Command implements PluginOwned
        $message = str_replace("{balance}", $args[1], $message);
 
        $sender->sendMessage($message);
-    return true;
+       return true;
     }
-
-
 }
