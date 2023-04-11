36329453280f24e468f762f7198e34e5cd1ed078
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-11 12:47:33 +0200

    Update Main.php

--- src/anybananagame/ottereco/Main.php
+++ src/anybananagame/ottereco/Main.php
@@ -1,7 +1,6 @@
 <?php
 namespace anybananagame\ottereco;
 
-
 use pocketmine\permission\DefaultPermissions;
 use pocketmine\permission\Permission;
 use pocketmine\player\Player;
@@ -19,29 +18,24 @@ class Main extends PluginBase implements Listener{
     private static self $instance;
     private EcoFunctions $EcoFunc;
 
-
-
-
     public static function getInstance(): Main {
         return self::$instance;
     }
+    
     public function onLoad(): void {
         self::$instance = $this;
     }
+
     public function onEnable(): void { 
         $this->saveDefaultConfig();
 
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->EcoFunc = new EcoFunctions($this);
 
-        $this->getServer()->getCommandMap()->registerAll("ottereco", [
-			new BalanceCommand($this),
-            new SetBalanceCommand($this),
-            new AddBalanceCommand($this),
-            new PayCommand($this)
-		]);
+        $this->getServer()->getCommandMap()->registerAll("ottereco", [ new BalanceCommand($this), new SetBalanceCommand($this), new AddBalanceCommand($this), new PayCommand($this) ]);
     }
-    public function onJoin(PlayerJoinEvent $event): void{
+
+    public function onJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
         $username = strtolower($player->getName());
         $path = $this->getDataFolder(). "database.json";
@@ -50,7 +44,8 @@ class Main extends PluginBase implements Listener{
 
         $lowuser = strtolower($username);
         $data = $db->get("$lowuser");
-        if(!$data){
+        
+	if (!$data) {
             $player->sendMessage("Database created!");
             $db->setDefaults([
                 "$username" => [
@@ -59,23 +54,13 @@ class Main extends PluginBase implements Listener{
             ]);           
             $db->save(); 
         }
-
-
     }
 
-    public function onBreak(BlockBreakEvent $event){
+    public function onBreak(BlockBreakEvent $event) {
             $player = $event->getPlayer();
     }
 
-
     public function getEcoFunc(): ? EcoFunctions {
 		return $this->EcoFunc ?? null;
-	}
-
-
-    
-
-
-
-
+    }
 }
