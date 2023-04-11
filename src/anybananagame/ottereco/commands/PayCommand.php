8ec3a12925ef92061deebde01b3c741b54511ed4
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-11 12:50:41 +0200

    Update PayCommand.php

--- src/anybananagame/ottereco/commands/PayCommand.php
+++ src/anybananagame/ottereco/commands/PayCommand.php
@@ -13,8 +13,8 @@ class PayCommand extends Command implements PluginOwned
 
     use PluginOwnedTrait;
     private $con;
-    public function __construct(Main $plugin)
-    {
+    
+    public function __construct(Main $plugin) {
         $this->owningPlugin = $plugin;
         $this->con = $plugin;
         parent::__construct("pay", "Pay someone money", "/pay <player> <amount>");
@@ -22,16 +22,17 @@ class PayCommand extends Command implements PluginOwned
         $this->setDescription("Pay someone else money");
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
-    {
-        if(!isset($args[0])) {
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        
+        if (!isset($args[0])) {
             $message = $this->con->getConfig()->get("nousername");
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
             $message = str_replace("{cmd}", "pay", $message);
             $sender->sendMessage($message);
             return true;
         } 
-        if(!isset($args[1])) {
+        
+        if (!isset($args[1])) {
             $message = $this->con->getConfig()->get("noamount");
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
             $message = str_replace("{cmd}", "pay", $message);
@@ -42,34 +43,36 @@ class PayCommand extends Command implements PluginOwned
         $receivertolow = strtolower($args[0]);
         $sendertolow = strtolower($sender->getName());
 
-
         $receiverdata = $db->get("$receivertolow");
-        if(!$receiverdata){
+        if (!$receiverdata){
             $message = $this->con->getConfig()->get("userdoesnotexist");
             $message = str_replace("{username}", $receivertolow, $message);
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
             $sender->sendMessage($message);
             return true;
         }
+        
         $receiversbal = $receiverdata["money"];
 
         $payerdata = $db->get($sendertolow);
         $payersbal = $payerdata["money"];
 
-        if(!is_numeric($args[1])){
+        if (!is_numeric($args[1])){
             $message = $this->con->getConfig()->get("NAN");
             $message = str_replace("{arg}", $args[1], $message);
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
             $sender->sendMessage($message);
             return true;
         }
-        if($payersbal < $args[1]){
+
+        if ($payersbal < $args[1]){
             $message = $this->con->getConfig()->get("notenoughmoney");
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
             $sender->sendMessage($message);
             return true;
         }
-        if($args[1] < 0){
+        
+        if ($args[1] < 0){
             $message = $this->con->getConfig()->get("belowzero");
             $message = str_replace("{arg}", $args[1], $message);
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
@@ -80,7 +83,7 @@ class PayCommand extends Command implements PluginOwned
         $this->con->getEcoFunc()->takeMoney($sendertolow, $args[1]);
         $this->con->getEcoFunc()->addMoney($receivertolow, $args[1]);
 
-        if($this->con->getServer()->getPlayerExact($receivertolow) != null){
+        if ($this->con->getServer()->getPlayerExact($receivertolow) != null){
             $player = $this->con->getServer()->getPlayerExact($receivertolow);
             $message = $this->con->getConfig()->get("getpaid");
             $message = str_replace("{prefix}", $this->con->getConfig()->get("prefix"), $message);
@@ -94,14 +97,6 @@ class PayCommand extends Command implements PluginOwned
         $message = str_replace("{amount}", $args[1], $message);
         $message = str_replace("{receiver}", $receivertolow, $message);
         $sender->sendMessage($message);
-
-
-
-
-        
-
         return false;
     }
-
-
 }
